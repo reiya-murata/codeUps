12c5c0f569cd44d58d6213864b775e4cdd236dbd
村田　玲耶 <f.zero39.m@gmail.com>
2023-07-07 22:56:19 +0900

    comit

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,35 @@ burger.addEventListener("click",()=>{
   burger.classList.toggle("active");
 });
 // burger menu
+
+// swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints:{
+767:{
+  slidesPerView:3
+},
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
