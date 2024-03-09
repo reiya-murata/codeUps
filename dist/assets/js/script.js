5d27df9682d1c9282eba92037f9184cf4dd1d5d3
村田　玲耶 <f.zero39.m@gmail.com>
2024-03-09 13:32:04 +0900

    comit

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -83,3 +83,59 @@ var swiper = new Swiper(".swiper__campaign", {
     prevEl: '.swiper-button-prev'
   }
 });
+function checkInView() {
+  $('.color').each(function () {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    // 要素が画面内にあるかどうかを判定
+    if (elementBottom > viewportTop && elementTop < viewportBottom) {
+      // 画面内にある場合の処理を記述
+      // 例: 背景色が画面に現れたら何か処理をする
+      $(this).trigger('inview');
+    }
+  });
+}
+
+// スクロールやリサイズ時に実行する関数
+$(window).on('scroll resize', function () {
+  checkInView();
+});
+
+// ページのロード時にも実行する
+$(document).ready(function () {
+  checkInView();
+});
+
+//要素の取得とスピードの設定
+var box = $('.colorbox'),
+  speed = 700;
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function () {
+  $(this).append('<div class="color"></div>');
+  var color = $(this).find($('.color')),
+    image = $(this).find('img');
+  var counter = 0;
+  image.css('opacity', '0');
+  color.css('width', '0%');
+  //inviewを使って背景色が画面に現れたら処理をする
+  color.on('inview', function () {
+    if (counter == 0) {
+      $(this).delay(200).animate({
+        'width': '100%'
+      }, speed, function () {
+        image.css('opacity', '1');
+        $(this).css({
+          'left': '0',
+          'right': 'auto'
+        });
+        $(this).animate({
+          'width': '0%'
+        }, speed);
+      });
+      counter = 1;
+    }
+  });
+});
